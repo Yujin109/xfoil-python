577434047cca3e9da3fb0a41498ad73f296c0799
Enrique Millan Valbuena <enrique.millanvalbuena@gmail.com>
2019-06-09 12:48:49 +0200

    WIP: correctly expose all the variables from the library

--- src/xfoil/__init__.py
+++ src/xfoil/__init__.py
@@ -16,3 +16,4 @@
 #   You should have received a copy of the GNU General Public License
 #   along with XFoil.  If not, see <https://www.gnu.org/licenses/>.
 from .xfoil import XFoil
+from . import model
